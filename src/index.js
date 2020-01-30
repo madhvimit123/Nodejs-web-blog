272d9b2bb072b65d5b0713f8a0d6b6033364808b
Madhvi <madhavi.mittal.1998@gmail.com>
2020-01-30 17:14:20 +0530

    read users, blogs

--- src/index.js
+++ src/index.js
@@ -13,21 +13,38 @@ app.use(express.json())
 app.post('/users',(req,res)=>{
     const user= new User(req.body)
     user.save().then(()=>{
-        res.send(user)
+        res.status(201).send(user)
     }).catch((e)=>{
         res.status(400).send(e)
     })
 })
+
+app.get('/users',(req,res)=>{
+    User.find({}).then((users)=>{
+        res.status(200).send(users)
+    }).catch((e)=>{
+        res.status(500).send()
+    })
+    
+})
+
 
 app.post('/blogs',(req,res)=>{
     const blog= new Blog(req.body)
     blog.save().then(()=>{
-        res.send(blog)
+        res.status(201).send(blog)
     }).catch((e)=>{
         res.status(400).send(e)
     })
 })
 
+app.get('/blogs',(req,res)=>{
+    Blog.find({}).then((blogs)=>{
+        res.status(200).send(blogs)
+    }).catch((e)=>{
+        res.status(500).send()
+    })
+})
 
 app.listen(port,()=>{
     console.log('Server up at port',port)
