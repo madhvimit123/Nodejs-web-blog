b23a8fe5d8005d330cb37843a145a677f17c55a5
Madhvi <madhavi.mittal.1998@gmail.com>
2020-01-30 21:57:52 +0530

    middleware

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,13 +1,15 @@
 const express=require('express')
 const User=require('../models/user')
+const auth=require('../middleware/auth')
 const router = new express.Router()
 
 
 router.post('/users',async (req,res)=>{
     const user= new User(req.body)
     try{
-    await user.save()
-    status(201).send(user)
+        await user.save()
+        const token = await user.generateAuthToken()
+        res.status(201).send({user,token})
     }
     catch(e){
         res.status(400).send(e)
@@ -17,20 +19,15 @@ router.post('/users',async (req,res)=>{
 router.post('/users/login',async(req,res)=>{
     try{
         const user= await User.findByCredentials(req.body.email,req.body.password)
-        res.send(user)
+        const token= await user.generateAuthToken()
+        res.send({user, token})
     }catch(e){
         res.status(400).send()
     }
 })
 
-router.get('/users',async (req,res)=>{
-    try{
-        const users= await User.find({})
-        res.status(200).send(users)
-    }catch(e){
-        res.status(500).send()
-    }
-
+router.get('/users/me',auth,async (req,res)=>{
+    res.send(req.user)
     
 })
 
